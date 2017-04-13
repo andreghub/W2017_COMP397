2c68f1b8398fc7aca404a3f95ef8e630cbc9cb20
andreghub <andreoliveira.it@gmail.com>
2017-04-13 16:23:21 -0400

    Initial Commit     - Screen rotation     - Health Regeneration added

---
+++ FinalProject/js/classes/sprites/Bullet.js
@@ -0,0 +1,28 @@
+(function (window) {
+
+    window.game = window.game || {}
+
+    function Bullet() {
+        this.initialize();
+    }
+
+    var p = Bullet.prototype = new createjs.Sprite();
+
+    p.Sprite_initialize = p.initialize;
+
+    p.speed = 500;
+    p.nextY = null;
+    p.shouldDie = false;
+
+    p.initialize = function () {
+        this.Sprite_initialize(spritesheet, "bullet");
+        this.paused = true;
+        this.rotation = 90;
+    }
+    p.reset = function () {
+        this.shouldDie = false;
+    }
+
+    window.game.Bullet = Bullet;
+
+}(window));
